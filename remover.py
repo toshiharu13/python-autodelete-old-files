80396877413ab896305733f258194226552194d0
Boyko Alexey <elorian@yandex.ru>
2021-11-08 10:38:45 +0300

    rename some vaeiables, add .env file, start writing mnt func

--- remover.py
+++ remover.py
@@ -4,14 +4,16 @@ import datetime
 import os
 import shutil
 
+from dotenv import load_dotenv
 
-def move_func(src_f, dst_f, count_move):
+
+def move_func(src_f, dst_f, count_move, src_list_of_files):
     """
     Функция переносит файлы и папки и возвращает их количество
     """
-    if src:
+    if src_list_of_files:
         print(f'Перенос файлов из {src_f} в папку {dst_f}')
-        for file in src:
+        for file in src_list_of_files:
             shutil.move(os.path.join(src_f, file), os.path.join(dst_f, file))
             print(f'{src_f}{file} перенесен')
             count_move += 1
@@ -21,6 +23,10 @@ def move_func(src_f, dst_f, count_move):
 
 
 def clear_old_func(dst_r, dst_f, count, count_f, count_er):
+    """
+    Функция удаляет старые файлы из папки хранилища и возвращает
+    статистику операции
+    """
     count_array = []
     for file in dst_r:
         # полный путь к обрабатываему файлу
@@ -51,22 +57,44 @@ def clear_old_func(dst_r, dst_f, count, count_f, count_er):
     return count_array
 
 
+def mount(arm, user, passwd):
+    """
+    Монтируем АРМ по шаре C$
+    """
+    os.system(f'sudo -S mount -t cifs //{arm}/c$ /home/boyko-ab/mnt/logs2/ '
+              f'--verbose -o user={user},password={passwd}')
+
+
+def unmount(arm):
+    """
+    Размонтируем
+    """
+    os.system('sudo -S umount /home/boyko-ab/mnt/logs2/')
+    print(f'Отключаемся от {arm}')
+
+
 if __name__ == '__main__':
-    src_f = r'/home/boyko-ab/Рабочий стол/to_work'  # папка источник
-    dst_f = '/home/boyko-ab/mnt/myfolder/work'  # папка приёмник
-    src = os.listdir(src_f)  # список файлов во временной папке to_work
-    dst_r = os.listdir(dst_f)  # список файлов в папке work
-    now = datetime.date.today().strftime("%d.%m.%Y")  # сегодняшнее число
-    count = 0  # счетчик для удаленных файлов
-    count_er = 0  # счетчик ошибок при удалении
-    count_f = 0  # счетчик удаленных папок
-    count_move = 0  # счетчик перенесенных из  паки "протей" в папку "ворк"
+    load_dotenv()
+    src_folder = os.getenv('SOURCE')  # папка источник
+    dst_folder = os.getenv('DESTINATION')  # папка приёмник
+    src_list_of_files = os.listdir(src_folder)  # список файлов во временной папке to_work
+    dst_list_of_files = os.listdir(dst_folder)  # список файлов в папке work
+    time_now = datetime.date.today().strftime("%d.%m.%Y")
+    count_del_files = 0  # счетчик для удаленных файлов
+    count_del_errors = 0  # счетчик ошибок при удалении
+    count_del_folders = 0  # счетчик удаленных папок
+    count_moved_to_dest = 0  # счетчик перенесенных из  паки "протей" в папку "ворк"
 
-    print(f'сегодня {now}')
-    print_count = move_func(src_f, dst_f, count_move)
-    print_array = clear_old_func(dst_r, dst_f, count, count_f, count_er)
 
-    print(f'перенесено из папки {src_f} {print_count} файлов и папок')
-    print(f'удалено из папки {dst_f} {print_array[0]} '
-          f'файлов и {print_array[1]} папок')
-    print(f'ошибок в удалении {print_array[2]}')
+    print(f'сегодня {time_now}')
+    value_of_moved = move_func(
+        src_folder, dst_folder, count_moved_to_dest, src_list_of_files
+    )
+    statistic_del_operation = clear_old_func(
+        dst_list_of_files, dst_folder, count_del_files, count_del_folders, count_del_errors
+    )
+
+    print(f'перенесено из папки {src_folder} {value_of_moved} файлов и папок')
+    print(f'удалено из папки {dst_folder} {statistic_del_operation[0]} '
+          f'файлов и {statistic_del_operation[1]} папок')
+    print(f'ошибок в удалении {statistic_del_operation[2]}')
